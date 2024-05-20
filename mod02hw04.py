541d8f621c016d748cdbfafa8cb60161c76b3269
Александр <169141339+alxdrII@users.noreply.github.com>
2024-05-20 08:13:22 +0300

    Update mod02hw04.py

--- mod02hw04.py
+++ mod02hw04.py
@@ -2,5 +2,5 @@ cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 
 cars_count = 0
 for car in cars:
-    print('Я езжу на автомабиле марки', car)
     cars_count += 10
+    print(cars_count, 'Я езжу на автомабиле марки', car)
