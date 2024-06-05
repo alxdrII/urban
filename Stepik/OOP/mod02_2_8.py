adf8928980e6259144a9a6adc702fd3268045518
Александр <169141339+alxdrII@users.noreply.github.com>
2024-06-05 18:08:02 +0300

    Update mod02_2_8.py

--- Stepik/OOP/mod02_2_8.py
+++ Stepik/OOP/mod02_2_8.py
@@ -1,4 +1,3 @@
-# https://stepik.org/lesson/701984/step/10?unit=702085
 class TreeObj:
     def __init__(self, indx, value=None):
         self.indx = indx
@@ -26,25 +25,21 @@ class TreeObj:
 class DecisionTree:
     @classmethod
     def predict(cls, root, x):
-        # obj = root
-        # if x[0]:
-        #     obj = obj.left
-        #     if x[1]:
-        #         obj = obj.left
-        #     else:
-        #         obj = obj.right
-        # else:
-        #     obj = obj.right
-        #     if x[2]:
-        #         obj = obj.left
-        #     else:
-        #         obj = obj.right
-
-        if root.value:
-            return root.value
+        obj = root
+        if x[0]:
+            obj = obj.left
+            if x[1]:
+                obj = obj.left
+            else:
+                obj = obj.right
         else:
-            return cls.predict(cls, root.left if x[root.indx] == True , x)
-        # return obj.value
+            obj = obj.right
+            if x[2]:
+                obj = obj.left
+            else:
+                obj = obj.right
+
+        return obj.value
 
     @classmethod
     def add_obj(cls, obj, node=None, left=True):
@@ -55,16 +50,3 @@ class DecisionTree:
                 node.right = obj
 
         return obj
-
-
-root = DecisionTree.add_obj(TreeObj(0))
-v_11 = DecisionTree.add_obj(TreeObj(1), root)
-v_12 = DecisionTree.add_obj(TreeObj(2), root, False)
-DecisionTree.add_obj(TreeObj(-1, "будет программистом"), v_11)
-DecisionTree.add_obj(TreeObj(-1, "будет кодером"), v_11, False)
-DecisionTree.add_obj(TreeObj(-1, "не все потеряно"), v_12)
-DecisionTree.add_obj(TreeObj(-1, "безнадежен"), v_12, False)
-
-x = [0, 1, 0]
-res = DecisionTree.predict(root, x) # будет программистом
-print(res)
