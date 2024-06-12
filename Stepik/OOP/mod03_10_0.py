cdfdd02ef1dfb3c0a318d923ac259ecb427b7b73
Александр <169141339+alxdrII@users.noreply.github.com>
2024-06-12 23:57:01 +0300

    Update mod03_10_0.py

--- Stepik/OOP/mod03_10_0.py
+++ Stepik/OOP/mod03_10_0.py
@@ -25,7 +25,10 @@ class TicTacToe:
 
     def __init__(self):
         self.pole = None
-        self.clear()
+        self.init()
+        # self.is_human_win = False     # - возвращает True, если победил человек, иначе - False
+        # self.is_computer_win = False  # - возвращает True, если победил компьютер, иначе - False
+        # self.is_draw = False          # - возвращает True, если ничья, иначе - False
 
     def __getitem__(self, item):
         self.checking(item)
@@ -33,25 +36,59 @@ class TicTacToe:
 
     def __setitem__(self, key, value):
         self.checking(key)
+        if not value in (self.COMPUTER_O, self.HUMAN_X):
+            raise ValueError("Передано неверное значение")
+
         self.pole[key[0]][key[1]].value = value
+
+    def __bool__(self):
+        """возвращает True, если игра не окончена (никто не победил и есть свободные клетки)
+        и False - в противном случае."""
+        return not any(self.is_human_win, self.is_computer_win, self.is_draw)
 
     @staticmethod
     def checking(value: tuple):
         if 0 >= value[0] >= 2 and 0 >= value[1] >= 2:
-            raise IndexError('некорректно указанные индексы')
+            raise IndexError('некорректно указанные индексы'
 
-    def clear(self):
+    def init(self):
         self.pole = tuple([tuple([Cell(self.FREE_CELL) for _ in range(3)]) for _ in range(3)])
+        self.is_human_win = False     # - возвращает True, если победил человек, иначе - False
+        self.is_computer_win = False  # - возвращает True, если победил компьютер, иначе - False
+        self.is_draw = False          # - возвращает True, если ничья, иначе - False
 
     def show(self):
+        print()
         for i in range(3):
             print(*self.pole[i])
 
+    def human_go(self):
+        """реализация хода игрока (запрашивает координаты свободной клетки и ставит туда крестик);"""
+        pass
 
-dd = TicTacToe()
-dd[0, 0] = 1
-dd[1, 0] = 2
-dd.show()
-dd[2, 0] = 555
-dd.show()
+    def computer_go(self):
+        """- реализация хода компьютера (ставит случайным образом нолик в свободную клетку)."""
+        pass
 
+
+game = TicTacToe()
+game.init()
+step_game = 0
+while game:
+    game.show()
+
+    if step_game % 2 == 0:
+        game.human_go()
+    else:
+        game.computer_go()
+
+    step_game += 1
+
+game.show()
+
+if game.is_human_win:
+    print("Поздравляем! Вы победили!")
+elif game.is_computer_win:
+    print("Все получится, со временем")
+else:
+    print("Ничья.")
