97cdc6eb7378d96588329e50a4da0ef9274a6dff
Александр <169141339+alxdrII@users.noreply.github.com>
2024-06-20 17:22:03 +0300

    Add files via upload

---
+++ module5hard.py
@@ -0,0 +1,80 @@
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = hash(password)
+        self.age = age
+
+    def __hash__(self):
+        return hash(self.nickname)
+
+    def __eq__(self, other):
+        return self.nickname == other.nickname
+
+
+class Video:
+    def __init__(self, title, duration, time_now=0, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = time_now
+        self.adult_mode = adult_mode
+
+    # def __eq__(self, other):
+    #     return self.title.upper() == other.title.upper()
+    #
+    # def __hash__(self):
+    #     return hash(self.title.upper())
+
+    def __contains__(self, item):
+        print("__contains__")
+        if item.upper() in self.title.upper():
+            return True
+
+
+class UrTube:
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None  # текущий пользователь
+
+    def log_in(self, login, password):
+        user_new = User(login, password, 0)
+        for user in self.users:
+            if user == user_new and hash(user.password) == hash(password):
+                self.current_user = user
+                break
+
+        else:
+            print(f"Пользователя {login} не существует")
+
+    def register(self, nickname, password, age):
+        user_new = User(nickname, password, age)
+        if user_new in self.users:
+            print(f"Пользователь {nickname} уже существует")
+
+        else:
+            self.users.append(user_new)
+            self.current_user = user_new
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, *args):
+        for obj in args:
+            if not isinstance(obj, Video):
+                continue
+
+            if not obj in self.videos:
+                self.videos.append(obj)
+
+    def get_videos(self, find_str: str) -> list:
+        result = []
+        for video in self.videos:
+            if find_str in video:
+                result.append(video)
+
+        return result
+
+    def watch_video(self, video_name):
+        pass
+
+
