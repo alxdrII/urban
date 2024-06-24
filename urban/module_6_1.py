6b7a2246902b3da881d5b4a8a17ec013d211e5bb
Александр <169141339+alxdrII@users.noreply.github.com>
2024-06-24 10:52:11 +0300

    Create module_6_1.py

---
+++ urban/module_6_1.py
@@ -0,0 +1,46 @@
+class Plant:
+    """ Растение """
+
+    def __init__(self, name, edible=False):
+        self.name = name
+        self.edible = edible  # съедобность
+
+
+class Flower(Plant):
+    """ Цветок """
+    def __init__(self, name, edible=False):
+        super().__init__(name, edible)
+
+
+class Fruit(Plant):
+    """ Фрукт """
+    def __init__(self, name, edible=True):
+        super().__init__(name, edible)
+
+
+class Animal:
+    """ Животное """
+
+    def __init__(self, name, alive=True, fed=False):
+        self.name = name
+        self.alive = alive  # живой
+        self.fed = fed  # накормленный
+
+    def eat(self, food: Plant):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
+class Mammal(Animal):
+    """ Млекопитающий """
+    pass
+
+
+class Predator(Animal):
+    """ Хищник """
+    pass
