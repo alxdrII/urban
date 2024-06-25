fa5793fbec7afda62dec01a7becb7e2f015f9a91
Александр <169141339+alxdrII@users.noreply.github.com>
2024-06-25 14:19:21 +0300

    Update module_6_1.py

--- urban/module_6_1.py
+++ urban/module_6_1.py
@@ -44,3 +44,19 @@ class Mammal(Animal):
 class Predator(Animal):
     """ Хищник """
     pass
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
