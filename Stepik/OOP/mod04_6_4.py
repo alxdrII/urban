a9355a3c2916216bc7ae1d7fe84e102cf2c6e548
Александр <169141339+alxdrII@users.noreply.github.com>
2024-06-25 21:30:42 +0300

    Update mod04_6_4.py

--- Stepik/OOP/mod04_6_4.py
+++ Stepik/OOP/mod04_6_4.py
@@ -22,3 +22,41 @@ class Integer(Digit):
         return False
 
 
+class Float(Digit):
+    def _check(self, value):
+        if isinstance(value, float):
+            return True
+
+        return False
+
+
+class Positive(Digit):
+    def _check(self, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            return True
+
+        return False
+
+
+class Negative(Digit):
+    def _check(self, value):
+        if isinstance(value, (int, float)) and value < 0:
+            return True
+
+        return False
+
+
+class PrimeNumber(Integer, Positive):
+    pass
+
+
+class FloatPositive(Float, Positive):
+    pass
+
+
+# x1 = Digit(0)
+# x2 = Digit(2.2)
+# x3 = Digit(-8.5)
+# x4 = Digit(False)
+
+in1 = Positive(-4)
