939885e0896a6d657fc5e1841b893a5483f39bc0
Александр <169141339+alxdrII@users.noreply.github.com>
2024-06-30 18:23:22 +0300

    Update module_3_1.py

--- urban/module_3_1.py
+++ urban/module_3_1.py
@@ -21,5 +21,3 @@ print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
 print(calls)
-
-
