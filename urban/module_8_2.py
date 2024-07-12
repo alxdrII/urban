1002b52ae118d1a7e081473ce93cd2ad376d0299
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-12 14:36:02 +0300

    Update module_8_2.py

--- urban/module_8_2.py
+++ urban/module_8_2.py
@@ -1,13 +1,11 @@
-def personal_sum(numbers: tuple):
-    if not isinstance(numbers, tuple):
-        raise TypeError
-
+def personal_sum(numbers):
     result = 0
     incorrect_data = 0
     for num in numbers:
         try:
             result += num
         except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {num}')
             incorrect_data += 1
 
     return result, incorrect_data
@@ -15,17 +13,20 @@ def personal_sum(numbers: tuple):
 def calculate_average(numbers):
     try:
         summ, count_err = personal_sum(numbers)
-        return summ / (len(numbers) - count_err)
+        count = len(numbers) - count_err
+        result = summ / count
 
     except ZeroDivisionError:
-        return 0
+        result = 0
 
     except TypeError:
         print('В numbers записан некорректный тип данных')
-        return None
+        result = None
+
+    return result
 
 
-sum_in = (10, 10, 'dd', 10, 'dd')
-print(calculate_average(sum_in))
-print(calculate_average('222'))
-print(calculate_average(222))
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
