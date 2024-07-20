f73c36e21836eb0d4103315d996542abb844c552
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-20 11:07:17 +0300

    Add files via upload

---
+++ urban/module_9_4.py
@@ -0,0 +1,41 @@
+from random import choice
+
+# ----- Lambda-функция: ----------
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+result = list(map(lambda x, y: x == y, first, second))
+print("---- Lambda-функция:")
+print(result)
+print()
+
+#--------- Замыкание: ----------
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for d in data_set:
+                file.write(str(d)+"\n")
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+print("---- Замыкание:  ")
+print("см файл example.txt")
+print()
+
+# _______ Метод __call__: --------------
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print("--- Метод __call__:")
+print(first_ball())
+print(first_ball())
+print(first_ball())
