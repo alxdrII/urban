3dc6b8f6d6d72d8ff490c04d60d42bb4df93b973
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-22 11:51:26 +0300

    Add files via upload

---
+++ urban/module_9_7.py
@@ -0,0 +1,23 @@
+def is_prime(func):
+    def wrappeer(*args, **kwargs):
+        result = func(*args, **kwargs)
+        this_prime = True
+        if result not in (-1, 0, 1):
+            for i in range(2, result):
+                if result % i == 0:
+                    this_prime = False
+                    break
+
+        print(['Составное', 'Простое'][this_prime])
+
+        return result
+
+    return wrappeer
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+print(sum_three(2, 3, 2))
