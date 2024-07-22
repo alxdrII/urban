0cf9eb0a19c85c6817c33450a61c5e03c752840b
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-22 12:52:05 +0300

    Update module_9_6.py

--- urban/module_9_6.py
+++ urban/module_9_6.py
@@ -1,13 +1,13 @@
 def all_variants(text):
-    for ln in range(len(text)):
-        for st in range(len(text) - ln):
-            print(st, ln, text[st:ln])
-
+    for ln in range(1, len(text)+1):
+        for i in range(len(text)):
+            if ln+i > len(text):
+                break
+            yield text[i:ln+i]
 
 
 all_variants("abc")
 
-# a = all_variants("abc")
-# for i in a:
-#     print(i)
-
+a = all_variants("abc")
+for i in a:
+    print(i)
