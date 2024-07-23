fa1758de593692a3119c61a881a5627da8a8f7ec
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-24 00:00:04 +0300

    Add files via upload

---
+++ Stepik/OOP/mod04_8_1.py
@@ -0,0 +1,97 @@
+class Vertex:
+    def __init__(self):
+        self._links = []
+
+    @property
+    def links(self):
+        return self._links
+
+
+class Station(Vertex):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+class Link:
+    def __init__(self, v1, v2, dist=1):
+        self._v1 = v1
+        self._v2 = v2
+        self._dist = dist
+
+    @property
+    def v1(self):
+        return self._v1
+
+    @property
+    def v2(self):
+        return self._v2
+
+    @property
+    def dist(self):
+        return self._dist
+
+    def __eq__(self, other):
+        print(other.v1)
+        return (self.v1 == other.v1 or self.v1 == other.v2) and (self.v2 == other.v1 or self.v2 == other.v2) and self.dist == other.dist
+
+    def __hash__(self):
+        return hash((hash(self.v1) * hash(self.v2), self.dist))
+
+
+class LinkMetro(Link):
+    def __init__(self, v1, v2, dist):
+        if dist <= 0:
+            raise ValueError('расстояние между станциями должно быть положительным')
+        super().__init__(v1, v2, dist)
+
+
+class LinkedGraph:
+    def __init__(self):
+        self._links = []
+        self._vertex = []
+
+    def add_vertex(self, v):
+        if v not in self._vertex:
+            self.add_link(v)
+
+    def add_link(self, link):
+        if link not in self._links:
+            self._links.append(link)
+            self.add_vertex(link.v1)
+            self.add_vertex(link.v2)
+
+    def find_path(self, start_v, stop_v):
+        return ([], [],)
+
+
+map_graph = LinkedGraph()
+
+v1 = Vertex()
+v2 = Vertex()
+v3 = Vertex()
+v4 = Vertex()
+v5 = Vertex()
+v6 = Vertex()
+v7 = Vertex()
+
+map_graph.add_link(Link(v1, v2))
+map_graph.add_link(Link(v2, v3))
+map_graph.add_link(Link(v1, v3))
+
+map_graph.add_link(Link(v4, v5))
+map_graph.add_link(Link(v6, v7))
+
+map_graph.add_link(Link(v2, v7))
+map_graph.add_link(Link(v3, v4))
+map_graph.add_link(Link(v5, v6))
+
+print(len(map_graph._links))   # 8 связей
+print(len(map_graph._vertex))  # 7 вершин
+path = map_graph.find_path(v1, v6)
