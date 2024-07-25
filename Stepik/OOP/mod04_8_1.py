fd9d055e2ce93c868beb3134f6032338bb342014
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-25 17:39:43 +0300

    Update mod04_8_1.py

--- Stepik/OOP/mod04_8_1.py
+++ Stepik/OOP/mod04_8_1.py
@@ -71,27 +71,54 @@ class LinkedGraph:
         return ([], [],)
 
 
-map_graph = LinkedGraph()
+# map_graph = LinkedGraph()
+#
+# v1 = Vertex()
+# v2 = Vertex()
+# v3 = Vertex()
+# v4 = Vertex()
+# v5 = Vertex()
+# v6 = Vertex()
+# v7 = Vertex()
+#
+# map_graph.add_link(Link(v1, v2))
+# map_graph.add_link(Link(v2, v3))
+# map_graph.add_link(Link(v1, v3))
+#
+# map_graph.add_link(Link(v4, v5))
+# map_graph.add_link(Link(v6, v7))
+#
+# map_graph.add_link(Link(v2, v7))
+# map_graph.add_link(Link(v3, v4))
+# map_graph.add_link(Link(v5, v6))
+#
+# print(len(map_graph._links))   # 8 связей
+# print(len(map_graph._vertex))  # 7 вершин
+# path = map_graph.find_path(v1, v6)
 
-v1 = Vertex()
-v2 = Vertex()
-v3 = Vertex()
-v4 = Vertex()
-v5 = Vertex()
-v6 = Vertex()
-v7 = Vertex()
+# -----------------
+map_metro = LinkedGraph()
+v1 = Station("Сретенский бульвар")
+v2 = Station("Тургеневская")
+v3 = Station("Чистые пруды")
+v4 = Station("Лубянка")
+v5 = Station("Кузнецкий мост")
+v6 = Station("Китай-город 1")
+v7 = Station("Китай-город 2")
 
-map_graph.add_link(Link(v1, v2))
-map_graph.add_link(Link(v2, v3))
-map_graph.add_link(Link(v1, v3))
+map_metro.add_link(LinkMetro(v1, v2, 1))
+map_metro.add_link(LinkMetro(v2, v3, 1))
+map_metro.add_link(LinkMetro(v1, v3, 1))
 
-map_graph.add_link(Link(v4, v5))
-map_graph.add_link(Link(v6, v7))
+map_metro.add_link(LinkMetro(v4, v5, 1))
+map_metro.add_link(LinkMetro(v6, v7, 1))
 
-map_graph.add_link(Link(v2, v7))
-map_graph.add_link(Link(v3, v4))
-map_graph.add_link(Link(v5, v6))
+map_metro.add_link(LinkMetro(v2, v7, 5))
+map_metro.add_link(LinkMetro(v3, v4, 3))
+map_metro.add_link(LinkMetro(v5, v6, 3))
 
-print(len(map_graph._links))   # 8 связей
-print(len(map_graph._vertex))  # 7 вершин
-path = map_graph.find_path(v1, v6)
+print(len(map_metro._links))
+print(len(map_metro._vertex))
+path = map_metro.find_path(v1, v6)  # от сретенского бульвара до китай-город 1
+print(path[0])    # [Сретенский бульвар, Тургеневская, Китай-город 2, Китай-город 1]
+print(sum([x.dist for x in path[1]]))  # 7
