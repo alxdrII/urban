43ab84958b543064f9905366d0f0e6c8405916f3
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-26 16:22:49 +0300

    Add files via upload

---
+++ Stepik/OOP/mod04_5_9.py
@@ -0,0 +1,19 @@
+class PointTrack:
+    """Хранит коортинату точки"""
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def __setattr__(self, key, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('координаты должны быть числами')
+
+        super().__setattr__(key, value)
+
+    def __str__(self):
+        return f"PointTrack: {self._x}, {self._y}"
+
+
+p = PointTrack(4, 5.5)
+print(p)
