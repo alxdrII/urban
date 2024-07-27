d53b403dceee92ce89f8c2be32defe14ae510189
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-27 20:45:39 +0300

    Update module_3_4.py

--- urban/module_3_4.py
+++ urban/module_3_4.py
@@ -1,6 +1,11 @@
 def single_root_words(root_word, *other_words):
-    same_words = filter(lambda x: x.lower() in root_word.lower() or root_word.lower() in x.lower(), other_words)
-    return list(same_words)
+    #same_words = list(filter(lambda x: x.lower() in root_word.lower() or root_word.lower() in x.lower(), other_words))
+    same_words = []
+    for o_word in other_words:
+        if o_word.lower() in root_word.lower() or root_word.lower() in o_word.lower():
+            same_words.append(o_word)
+
+    return same_words
 
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
