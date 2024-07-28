3fd1710505b0594cda8b1bf2b3a4a9d36b375cbf
Александр <169141339+alxdrII@users.noreply.github.com>
2024-07-28 09:59:42 +0300

    Add files via upload

--- Stepik/OOP/mod04_1_9.py
+++ Stepik/OOP/mod04_1_9.py
@@ -3,21 +3,31 @@ class Layer:
         self.next_layer = None
         self.name = 'Layer'
 
+    def __call__(self, layer):
+        self.next_layer = layer
+        return layer
+
 
 class Input(Layer):
     def __init__(self, inputs):
         super().__init__()
-        self.inputs = inputs
         self.name = 'Input'
+        for i in range(inputs):
+            self()
 
 
 class Dense(Layer):
     def __init__(self, inputs, outputs, activation):
         super().__init__()
-        self.inputs = inputs          # число входов в слой
-        self.outputs = outputs        # число выходов слоя (целые числа)
-        self.activation = activation  # функция активации (строка, например: 'linear', 'relu', 'sigmoid')
-        self.name = "Dense"
+        self.name = 'Dense'
 
 
 class NetworkIterator:
+    def __init__(self, network):
+        self.network = network
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        pass
