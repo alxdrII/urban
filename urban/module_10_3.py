953715508f7dd76a1978c16192662a2a27689a88
Александр <169141339+alxdrII@users.noreply.github.com>
2024-08-01 21:50:37 +0300

    Add files via upload

---
+++ urban/module_10_3.py
@@ -0,0 +1,42 @@
+from threading import Thread, Lock
+
+
+class BankAccount:
+    def __init__(self, balance=0):
+        super().__init__()
+        self.__balance = balance
+        self.__lock = Lock()
+
+    def deposit(self, amount):
+        with self.__lock:
+            self.__balance += amount
+
+        print(f"Deposited {amount}, new balance is {self.__balance}")
+
+    def withdraw(self, amount):
+        with self.__lock:
+            self.__balance -= amount
+
+        print(f"Withdrew {amount}, new balance is {self.__balance}")
+
+
+def deposit_task(account, amount):
+  for _ in range(5):
+    account.deposit(amount)
+
+def withdraw_task(account, amount):
+  for _ in range(5):
+    account.withdraw(amount)
+
+
+account = BankAccount(1000)
+
+deposit_thread = Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
+
