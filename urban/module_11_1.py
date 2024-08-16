908b4de50d1e66fb8bfcdcef0ee7fa696beb67cb
Александр <169141339+alxdrII@users.noreply.github.com>
2024-08-16 13:44:59 +0300

    Update module_11_1.py

--- urban/module_11_1.py
+++ urban/module_11_1.py
@@ -1,7 +1,4 @@
 import requests
-from PIL import Image
-import glob, os
-import tkinter.filedialog as fd
 
 
 def place_by_zip(zip: int) -> str:
@@ -15,31 +12,7 @@ def place_by_zip(zip: int) -> str:
         return ""
 
 
-def resize_picture():
-    """Изменяет размеры всех рисунков *.jpeg в выбранной папке"""
-
-    directory = fd.askdirectory(title="Открыть папку с картинками", initialdir="./")
-    if not directory:
-        return -1
-
-    size = 128, 128
-    count = 0
-    for infile in glob.glob(directory + "/*.jpeg"):
-        file, ext = os.path.splitext(infile)
-        with Image.open(infile) as im:
-            im.thumbnail(size)
-            im.save(file + "_sm" + ".jpg", "JPEG")
-            count += 1
-
-    return count
-
-
 print("--- Работа с библиотекой 'requests': получаем данные с сайта 'https://kladr-api.ru/api.php' по индексу:")
 print(place_by_zip(601501))
 print(place_by_zip(170001))
 print(place_by_zip(173001))
-print()
-
-print("--- Работа с библиотекой 'PIL': Меняем размер файлов .jpeg")
-count = resize_picture()
-print(f"Изменен размер у {count} рисунков.")
