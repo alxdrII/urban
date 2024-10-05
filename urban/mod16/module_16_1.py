3a67d26d75f71a645bdc7e190cca81920bc68520
Александр <169141339+alxdrII@users.noreply.github.com>
2024-10-05 23:11:20 +0300

    Create module_16_1.py

---
+++ urban/mod16/module_16_1.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome():
+    return "Главная страница"
+
+
+@app.get("/user/admin")
+async def admin():
+    return "Вы вошли как администратор"
+
+
+@app.get("/user/{user_id}")
+async def user_id(user_id):
+    return f"Вы вошли как пользователь № {user_id}"
+
+
+@app.get("/user")
+async def users_info(username: str, age: str):
+    return f"Информация о пользователе. Имя: {username}, Возраст {age}"
